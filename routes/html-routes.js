91e68202c31ca8b57cf3314224d0a550338cd052
jessny <jessnyjoseph@gmail.com>
2021-02-18 22:16:46 -0600

    added exercise html route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,9 +5,9 @@ router.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-// router.get("/", function(req, res) {
-//     res.sendFile(path.join(__dirname, "../public/index.html"));
-// });
+router.get("/exercise", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
 
 
 module.exports = router;
