4a3d6cbf7f7dd8b22c52ba2dc78651e3222fa808
jessny <jessnyjoseph@gmail.com>
2021-02-18 22:18:05 -0600

    added stats html route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -9,5 +9,9 @@ router.get("/exercise", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
+router.get("/stats", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
 
 module.exports = router;
