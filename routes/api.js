59148b977ee625f029a16d24d0a446435aab09d6
jessny <jessnyjoseph@gmail.com>
2021-02-18 21:58:14 -0600

    added post route for api/workouts

---
+++ routes/api.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+const Transaction = require("../models/transaction.js");
+
+router.post("/api/workouts", ({body}, res) => {
+    Workout.create(body)
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
